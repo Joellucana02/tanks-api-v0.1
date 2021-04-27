792191d91f760e7668aeeb23fe700ba197bab8c7
joel <joellucana02@gmail.com>
2021-04-27 16:15:40 -0300

    cors...

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const importData = require('./tank-data.json');
 let PORT = process.env.PORT || 5555;
 
 app.get("/", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
     res.send("OK");
 });
 app.get("/finally", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
     res.send(importData);
 });
 app.listen(PORT, () => {
