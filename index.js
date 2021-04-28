b2b71b416d070147c96992ca7f7e39e1c60a4265
joel <joellucana02@gmail.com>
2021-04-28 11:24:09 -0300

    ...

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const importData = require('./tank-data.json');
 let PORT = process.env.PORT || 5555;
-header("Access-Control-Allow-Origin: *");
+
 app.get("/", (req, res) => {
+    res.header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Origin: *");
     res.send("OK");
     res.set("Content-Type", "text/html");
     res.append("Access-Control-Allow-Origin", "*");
 });
 app.get("/finally", (req, res) => {
+    res.header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Origin: *");
     res.send(importData);
     res.set("Content-Type", "text/html");
