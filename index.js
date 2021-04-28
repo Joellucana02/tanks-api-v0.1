6f300bb16ef85f1b4795478e11f6e54a6d554a61
joel <joellucana02@gmail.com>
2021-04-28 10:55:26 -0300

    I added the same two lines...

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ let PORT = process.env.PORT || 5555;
 app.get("/", (req, res) => {
     res.set("Content-Type", "text/html");
     res.append("Access-Control-Allow-Origin", "*");
-
     res.send("OK");
 });
 app.get("/finally", (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-
+    res.set("Content-Type", "text/html");
+    res.append("Access-Control-Allow-Origin", "*");
     res.send(importData);
 });
 app.listen(PORT, () => {
