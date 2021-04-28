124c3ca3b6564cb3bca8e4ccd95fcd0ea41e1fe3
joel <joellucana02@gmail.com>
2021-04-28 11:21:27 -0300

    Theres at least five lines that are the same, why its not running?

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 const importData = require('./tank-data.json');
 let PORT = process.env.PORT || 5555;
-
+header("Access-Control-Allow-Origin: *");
 app.get("/", (req, res) => {
-
+    header("Access-Control-Allow-Origin: *");
     res.send("OK");
     res.set("Content-Type", "text/html");
     res.append("Access-Control-Allow-Origin", "*");
 });
 app.get("/finally", (req, res) => {
-
+    header("Access-Control-Allow-Origin: *");
     res.send(importData);
     res.set("Content-Type", "text/html");
     res.append("Access-Control-Allow-Origin", "*");
