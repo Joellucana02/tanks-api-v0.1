e79dd18c5a3e50f3dddd4bdabe56487f1f4b0eeb
joel <joellucana02@gmail.com>
2021-04-30 07:23:06 -0300

    ... ... ...

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const importData = require('./tank-data.json');
 let PORT = process.env.PORT || 5555;
 
 app.get("/", (req, res) => {
-    res.header("Access-Control-Allow-Origin: *");
+    res.set("Content-Type", "text/html");
+    res.append("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     res.send("OK");
-    res.set("Content-Type", "text/html");
-    res.append("Access-Control-Allow-Origin", "*");
 });
 app.get("/finally", (req, res) => {
-    res.header("Access-Control-Allow-Origin: *");
-
-    res.send(importData);
     res.set("Content-Type", "text/html");
     res.append("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    res.send(importData);
 });
 app.listen(PORT, () => {
     console.log(`App is starting on port http://localhost:${PORT}`);
