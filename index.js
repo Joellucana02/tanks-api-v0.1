2870e42f03be1272a1cbfd6adf7de724985a5f2e
joel <joellucana02@gmail.com>
2021-04-30 07:46:22 -0300

    ... ... ... ...

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const importData = require('./tank-data.json');
-let PORT = process.env.PORT || 5555;
+let PORT = process.env.PORT || 5500;
 
 app.get("/", (req, res) => {
-    res.set("Content-Type", "text/html");
-    res.append("Access-Control-Allow-Origin", "*");
+
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     res.send("OK");
 });
 app.get("/finally", (req, res) => {
-    res.set("Content-Type", "text/html");
-    res.append("Access-Control-Allow-Origin", "*");
+
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
